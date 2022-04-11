f33f8acefded0126de3f0089515ee1be7d23c351
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-11 11:49:50 -0500

    [CREATE]

---
+++ 0x00-ls/main.c
@@ -0,0 +1,23 @@
+/*
+* main.c - emulation of ls command
+* Author: Rodrigo Zárate Algecira
+* Date: April 11, 2022
+*/
+
+#include "head.h"
+
+/**
+* main - my own ls command
+* Return: exit Success or Failure
+*/
+
+int main()
+{
+	int allfine = 1;
+
+	if (allfine)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	exit(EXIT_FAILURE);
+}

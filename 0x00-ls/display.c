668e19fb4fb236be666171a7e1a2e0f69b6cdfe6
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-16 02:05:51 -0500

    [FIX] mem leak

--- 0x00-ls/display.c
+++ 0x00-ls/display.c
@@ -15,10 +15,14 @@ void print_full(struct dirent *read)
 	struct stat fileStat;
 	struct group *grp;
 	struct passwd *user;
-	char time[24];
+	char *time;
 
-	_strncpy(time, ctime(&fileStat.st_mtime), 24);
 	lstat((*read).d_name, &fileStat);
+	user = getpwuid(fileStat.st_uid);
+	grp = getgrgid(fileStat.st_gid);
+	time = ctime(&(fileStat.st_mtime));
+	time[16] = '\0';
+	time = time + 4;
 
 	printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
 	printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
@@ -30,12 +34,10 @@ void print_full(struct dirent *read)
 	printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
 	printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
 	printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
-
-	user = getpwuid(fileStat.st_uid);
-	grp = getgrgid(fileStat.st_gid);
-
-	printf(" %lu %s %s %li %s %s\n", fileStat.st_nlink, (*user).pw_name,
-	(*grp).gr_name, (intmax_t)fileStat.st_size, time, (*read).d_name);
+	printf(" %lu", fileStat.st_nlink);
+	printf(" %s %s", (*user).pw_name, (*grp).gr_name);
+	printf(" %li",  (intmax_t)fileStat.st_size);
+	printf(" %s %s\n", time, (*read).d_name);
 }
 
 /**

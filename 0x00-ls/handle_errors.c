21045b8f4c52fca6018c45d7b2add93eecefac35
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-16 13:05:49 -0500

    [FIX]

--- 0x00-ls/handle_errors.c
+++ 0x00-ls/handle_errors.c
@@ -15,5 +15,5 @@ void handle_errors(char *directory, char const *prog_name)
 	else if (errno == EACCES)
 		fprintf(stderr, "%s: cannot open directory %s: ",
 			prog_name, directory);
-	perror("");
+	/* perror(""); */
 }

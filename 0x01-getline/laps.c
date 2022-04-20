8cbfd6740110b2c5abb813947a4a508b5ac63633
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-20 16:08:56 -0500

    [UPDATE]

--- 0x01-getline/laps.c
+++ 0x01-getline/laps.c
@@ -1,1 +1,39 @@
-some lines
+#include "laps.h"
+
+/**
+ * race_state - Keep track of the number of laps made by several cars in a race
+ * id: array of int identifier of a car (unique)
+ * size: the size of the array
+ */
+void race_state(int *id, size_t size)
+{
+    if (size == 0){
+        free_memory();
+    }
+    // get array
+    // store array element if not in keys
+    // if new element add node else print state and sum 1
+    // if identifier is unknown
+    // create a new car with laps = 0
+    // Print Car X joined the race \n X = identifier
+    // Each time the function is called 
+    // number of laps +1 for all cars in identifier
+    // print state of race
+    // HEADER Race state: \n
+    // Car X [Y laps]
+    // if functions is called with size = 0 free all memory
+}
+
+cardef_t *add_car(cardef_t **head, int id)
+{
+
+}
+cardef_t *get_car(cardef_t *head, int id){
+
+}
+void print_car(cardef_t *head){
+
+}
+void free_car(cardef_t *head){
+    
+}
--- 0x01-getline/laps.h
+++ 0x01-getline/laps.h
@@ -1,6 +1,23 @@
 #ifndef _LAPS_H_
 #define _LAPS_H_
 
+/**
+* struct cardef - definition of car
+* @id: id of car
+* @laps: laps count
+* @next: pointer
+*/
+typedef struct cardef_s
+{
+    int id;
+    int laps;
+    struct cardef_s *next;
+} cardef_t;
 
+void race_state(int *id, size_t size);
+cardef_t *add_car(cardef_t **head, int id);
+cardef_t *get_car(cardef_t *head, int id);
+void print_car(cardef_t *head);
+void free_car(cardef_t *head);
 
 #endif

d1a20c9a46ac2effb4cd12d60ba2e3c3ab136521
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-20 19:22:23 -0500

    [FIX] Betty style guide spaces vs tabs due to vscode

--- 0x01-getline/laps.c
+++ 0x01-getline/laps.c
@@ -2,45 +2,46 @@
 
 /**
  * race_state - Keep track of the number of laps made by several cars in a race
- * id: array of int identifier of a car (unique)
- * size: the size of the array
+ * @id: array of int identifier of a car (unique)
+ * @size: the size of the array
  */
 void race_state(int *id, size_t size)
 {
-    size_t i;
-    static cardef_t *head;
-    cardef_t *newcar;
+	size_t i;
+	static cardef_t *head;
+	cardef_t *newcar;
 
-    /* if functions is called with size = 0 free all memory */
-    if (size == 0){
-        free_car(head);
-        return;
-    }
-    /* get array */
-    for (i = 0; i < size; i++)
-    {
-        newcar = get_car(head, id[i]);
-        /* if new element add node else print state and sum 1 */
-        /* number of laps +1 for all cars in identifier */
-        if (newcar)
-        {
-            newcar->laps++;
-        }
-        else
-        {
-            /* if identifier is unknown */
-            /* store array element if not in keys */
-            add_car(&head, id[i]);
-            /* create a new car with laps = 0 */
-            /* Print Car X joined the race \n X = identifier */
-            printf("Car %d joined the race\n", id[i]);
+	/* if functions is called with size = 0 free all memory */
+	if (size == 0)
+	{
+		free_car(head);
+		return;
+	}
+	/* get array */
+	for (i = 0; i < size; i++)
+	{
+		newcar = get_car(head, id[i]);
+		/* if new element add node else print state and sum 1 */
+		/* number of laps +1 for all cars in identifier */
+		if (newcar)
+		{
+			newcar->laps++;
+		}
+		else
+		{
+			/* if identifier is unknown */
+			/* store array element if not in keys */
+			add_car(&head, id[i]);
+			/* create a new car with laps = 0 */
+			/* Print Car X joined the race \n X = identifier */
+			printf("Car %d joined the race\n", id[i]);
 
-        }
-    }
-    /* Each time the function is called */
-    /* print state of race */
-    /* HEADER Race state: \n */
-    print_race(head);
+		}
+	}
+	/* Each time the function is called */
+	/* print state of race */
+	/* HEADER Race state: \n */
+	print_race(head);
 }
 
 /**
@@ -51,34 +52,34 @@ void race_state(int *id, size_t size)
  */
 cardef_t *add_car(cardef_t **head, int id)
 {
-    cardef_t *newnode, *car;
+	cardef_t *newnode, *car;
 
-    newnode = malloc(sizeof(*newnode));
-    if (!newnode)
-        return (NULL);
-    newnode->id = id;
-    newnode->laps = 0;
-    newnode->next = NULL;
+	newnode = malloc(sizeof(*newnode));
+	if (!newnode)
+		return (NULL);
+	newnode->id = id;
+	newnode->laps = 0;
+	newnode->next = NULL;
 
-    if (*head == NULL || (*head)->id > id)
-    {
-        newnode->next = *head;
-        *head = newnode;
-        return (newnode);
-    }
+	if (*head == NULL || (*head)->id > id)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
 
-    car = *head;
-    while (car->next)
-    {
-        if (car->next->id >= id)
-        {
-            newnode->next = car->next;
-            break;
-        }
-        car = car->next;
-    }
-    car->next = newnode;
-    return (newnode);
+	car = *head;
+	while (car->next)
+	{
+		if (car->next->id >= id)
+		{
+			newnode->next = car->next;
+			break;
+		}
+		car = car->next;
+	}
+	car->next = newnode;
+	return (newnode);
 }
 
 /**
@@ -89,12 +90,12 @@ cardef_t *add_car(cardef_t **head, int id)
  */
 cardef_t *get_car(cardef_t *head, int id)
 {
-    cardef_t *car;
+	cardef_t *car;
 
-    for (car = head; car; car = car->next)
-        if (car->id == id)
-            break;
-    return (car);
+	for (car = head; car; car = car->next)
+		if (car->id == id)
+			break;
+	return (car);
 }
 
 /**
@@ -103,12 +104,12 @@ cardef_t *get_car(cardef_t *head, int id)
  */
 void print_race(cardef_t *head)
 {
-    cardef_t *car;
+	cardef_t *car;
 
-    printf("Race state:\n");
-        for (car = head; car; car = car->next)
-            /* Car X [Y laps] */
-            printf("Car %d [%d laps]\n", car->id, car->laps);
+	printf("Race state:\n");
+		for (car = head; car; car = car->next)
+			/* Car X [Y laps] */
+			printf("Car %d [%d laps]\n", car->id, car->laps);
 }
 
 /**
@@ -117,7 +118,7 @@ void print_race(cardef_t *head)
  */
 void free_car(cardef_t *head)
 {
-    if (!head)
+	if (!head)
 		return;
 	free_car(head->next);
 	free(head);
--- 0x01-getline/laps.h
+++ 0x01-getline/laps.h
@@ -4,16 +4,16 @@
 #include <stdlib.h>
 
 /**
-* struct cardef - definition of car
+* struct cardef_s - definition of car
 * @id: id of car
 * @laps: laps count
 * @next: pointer
 */
 typedef struct cardef_s
 {
-    int id;
-    int laps;
-    struct cardef_s *next;
+	int id;
+	int laps;
+	struct cardef_s *next;
 } cardef_t;
 
 void race_state(int *id, size_t size);

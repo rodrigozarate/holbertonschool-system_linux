1b1a84fc73ae7b296d2ad99c5e02ad6e6bd32eb9
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-04-23 21:16:18 -0500

    [UPDATE]

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -1,1 +1,34 @@
-some lines
+#include "_getline.h"
+
+/**
+ *  _getline - My own getline
+ * @fd: the file descriptor
+ * Return: string or NULL on error
+ */
+char *_getline(const int fd)
+{
+	 Read buffer via get
+	 Put in memory 
+	 Move until Reach the end 
+	 Free memory 
+}
+
+fdes_t getnode(const int fd)
+{
+
+}
+
+fdes_t *addnode(fdes_t **head, const int fd)
+{
+
+}
+
+char *movenode(char *newplace, char *origin)
+{
+
+}
+
+void freenode(fdes_t *head)
+{
+
+}
--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -1,6 +1,28 @@
-#ifndef _LAPS_H_
-#define _LAPS_H_
+#ifndef _MY_GETLINE_H_
+#define _MY_GETLINE_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
+#define READ_SIZE 2
+
+/**
+ * struct fdes_s - struct
+ * @fd: file descriptor
+ * @inbuf: info in buffer
+ * @buffer: buffer
+ * @next: next structure 
+ */
+typedef struct fdes_s
+{
+	int fd;
+	ssize_t inbuf;
+	char buffer[READ_SIZE];
+	struct fdes_s *next;
+} fdes_t;
+
+char *_getline(const int fd);
 
 #endif

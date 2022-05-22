4fac8d30a3a948fe39e0c47d5e40c760846c523f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-05-21 20:39:03 -0500

    [FIX] pep8

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -4,45 +4,45 @@
 from sys import argv, exit
 import pprint
 if __name__ == "__main__":
-	if len(argv) != 4:
-		print("Usage: {} pid search_string replace_string".format(argv[0]))
-		exit(1)
-	[pid, search, replace] = argv[1:]
-	if "" in [pid, search]:
-		print("Missing value")
-		exit(1)
-	if replace == '':
-		replace = ' ' * len(search)
+    if len(argv) != 4:
+        print("Usage: {} pid search_string replace_string".format(argv[0]))
+        exit(1)
+    [pid, search, replace] = argv[1:]
+    if "" in [pid, search]:
+        print("Missing value")
+        exit(1)
+    if replace == '':
+        replace = ' ' * len(search)
 
-	maps_path = "/proc/{}/maps".format(pid)
-	mem_path = "/proc/{}/mem".format(pid)
+    maps_path = "/proc/{}/maps".format(pid)
+    mem_path = "/proc/{}/mem".format(pid)
 
-	heap = {
-		"start": '',
-		"end": '',
-		"mode": "",
-		"mem": ""
-	}
-	with open(maps_path) as maps:
-		for line in maps:
-			if "[heap]" in line:
-				tmp = line.split()
-				tmp[0] = tmp[0].split('-')
-				tmp[0] = list(map(lambda x: int('0x' + x, 16), tmp[0]))
-				[heap['start'], heap['end']] = tmp[0]
-				heap['mode'] = tmp[1]
-				break
-	if heap["start"] == '':
-		print("Missing heap")
-		exit(2)
+    heap = {
+        "start": '',
+        "end": '',
+        "mode": "",
+        "mem": ""
+    }
+    with open(maps_path) as maps:
+        for line in maps:
+            if "[heap]" in line:
+                tmp = line.split()
+                tmp[0] = tmp[0].split('-')
+                tmp[0] = list(map(lambda x: int('0x' + x, 16), tmp[0]))
+                [heap['start'], heap['end']] = tmp[0]
+                heap['mode'] = tmp[1]
+                break
+    if heap["start"] == '':
+        print("Missing heap")
+        exit(2)
 
-	with open(mem_path, 'rb+') as mem:
-		mem.seek(heap['start'])
-		heap["mem"] = mem.read(heap['end'] - heap['start'])
-		try:
-			i = heap["mem"].index(bytes(search, "ASCII"))
-		except ValueError:
-			print("Not matching string")
-			exit(3)
-		mem.seek(heap['start'] + i)
-		mem.write(bytes(replace, "ASCII"))
+    with open(mem_path, 'rb+') as mem:
+        mem.seek(heap['start'])
+        heap["mem"] = mem.read(heap['end'] - heap['start'])
+        try:
+            i = heap["mem"].index(bytes(search, "ASCII"))
+        except ValueError:
+            print("Not matching string")
+            exit(3)
+        mem.seek(heap['start'] + i)
+        mem.write(bytes(replace, "ASCII"))

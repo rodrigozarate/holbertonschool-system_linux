bad17e55a863baa2951ac163426e4bb616f76f0c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-05-29 15:28:15 -0500

    [UPDATE]

--- 0x04-readelf/header.h
+++ 0x04-readelf/header.h
@@ -1,6 +1,15 @@
 #ifndef _ELF_READER_
 #define _ELF_READER_
 
+#include <elf.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 /**
  * struct initvars - initial values to pass across the program
  * @p_name
@@ -10,7 +19,15 @@
 typedef struct initvars_s {
 	char *p_name;
 	char *f_name;
-	bool elf_type;
+	_Bool elf_type;
+	FILE *f_stream;
 } initvars_t;
+
+void initvars(initvars_t *state);
+void errorhandler(char *format, char *errorstring, initvars_t *state);
+void freewilli(initvars_t *state);
+int openfile(initvars_t *state);
+int getfileheader(initvars_t *state);
+int printfileheader(initvars_t *state);
 
 #endif /* _ELF_READER_ */

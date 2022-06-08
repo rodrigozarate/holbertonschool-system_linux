4c58994476f14c6e4f9ed9061fcd1088434b8ac5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-07 22:25:39 -0500

    [CREATE]

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -1,3 +1,20 @@
-size_t asm_strlen(const char *str)
-{
-}
+BITS 64
+
+global asm_strlen
+asm_strlen:
+	push	rbp
+	mov		rbp, rsp
+	mov		QWORD [rbp - 24], rdi
+	mov 	QWORD [rbp - 8], 0
+	jmp		.tests_null
+.str_len_plus:
+	add		QWORD [rbp - 8], 1
+	add		QWORD [rbp - 24], 1
+.tests_null:
+	mov		rax, QWORD [rbp - 24]
+	movzx	eax, BYTE [rax]
+	test	al, al
+	jne		.str_len_plus
+	mov		rax, QWORD [rbp - 8]
+	pop		rbp
+	ret

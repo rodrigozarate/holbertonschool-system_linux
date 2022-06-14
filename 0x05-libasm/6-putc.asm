5436620454933a8d1e38b14e317fa4c9faa62e14
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-14 01:36:09 -0500

    [UPDATE]

--- 0x05-libasm/6-putc.asm
+++ 0x05-libasm/6-putc.asm
@@ -1,13 +1,17 @@
-/* 
-* For this task, you are allowed to use the syscall instruction
-* only once in your file
-*/
+BITS 64
 
-/**
-* asm_putc - put c copycat
-* @c: char
-* Return: size_t
-*/
-size_t asm_putc(int c)
-{
-}
+global asm_putc
+
+asm_putc:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	mov DWORD [rbp - 4], edi
+	lea rax, [rbp - 4]
+	mov edx, 1
+	mov rsi, rax
+	mov edi, 1
+	mov eax, 1
+	syscall
+	leave
+	ret

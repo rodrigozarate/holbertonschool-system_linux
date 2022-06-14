05beeb3a035d84371d7e165f55abb0d9b71f4665
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-14 00:12:46 -0500

    [UPDATE]

--- 0x05-libasm/2-strncmp.asm
+++ 0x05-libasm/2-strncmp.asm
@@ -1,3 +1,56 @@
-int asm_strncmp(const char *s1, const char *s2, size_t n)
-{
-}
+BITS 64
+
+global asm_strncmp
+
+asm_strncmp:
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rbp - 40], rdi
+	mov QWORD [rbp - 32], rsi
+	mov QWORD [rbp - 24], rdx
+	mov QWORD [rbp - 16], 0
+	mov QWORD [rbp - 8], 0
+	jmp s1_s2_n_null
+walk_str:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	movsx edx, al
+	mov rax, QWORD [rbp - 32]
+	movzx eax, BYTE [rax]
+	movsx eax, al
+	sub edx, eax
+	mov eax, edx
+	mov QWORD [rbp - 16], eax
+	add QWORD [rbp - 24], 1
+	add QWORD [rbp - 32], 1
+	add QWORD [rbp - 8], 1
+s1_s2_n_null:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	test al, al
+	jne n_eq_test
+	mov rax, QWORD [rbp - 32]
+	movzx eax, BYTE [rax]
+	test al, al
+	je eq_test_may
+n_eq_test:
+	cmp QWORD [rbp - 16], 0
+	jne eq_test_may
+	mov rax, QWORD [rbp - 8]
+	cmp rax, QWORD [rbp - 40]
+	jb walk_str
+eq_test_may:
+	cmp QWORD [rbp - 16], 0
+	jle eq_test_min
+	mov eax, 1
+	jmp end_1
+eq_test_min:
+	cmp QWORD [rbp - 16], 0
+	jns end_0
+	mov eax, -1
+	jmp end_1
+end_0:
+	mov eax, 0
+end_1:
+	pop rbp
+	ret

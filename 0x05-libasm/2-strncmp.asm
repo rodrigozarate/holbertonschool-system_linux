489ea961c8225f126ffd5fea36e4613f68b53215
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-14 00:36:07 -0500

    [UPDATE]

--- 0x05-libasm/2-strncmp.asm
+++ 0x05-libasm/2-strncmp.asm
@@ -13,24 +13,24 @@ asm_strncmp:
 	jmp s1_s2_n_null
 walk_str:
 	mov rax, QWORD [rbp - 24]
-	movzx eax, BYTE [rax]
-	movsx edx, al
+	movzx rax, BYTE [rax]
+	movsx rdx, al
 	mov rax, QWORD [rbp - 32]
-	movzx eax, BYTE [rax]
-	movsx eax, al
-	sub edx, eax
-	mov eax, edx
-	mov QWORD [rbp - 16], eax
+	movzx rax, BYTE [rax]
+	movsx rax, al
+	sub rdx, rax
+	mov rax, rdx
+	mov QWORD [rbp - 16], rax
 	add QWORD [rbp - 24], 1
 	add QWORD [rbp - 32], 1
 	add QWORD [rbp - 8], 1
 s1_s2_n_null:
 	mov rax, QWORD [rbp - 24]
-	movzx eax, BYTE [rax]
+	movzx rax, BYTE [rax]
 	test al, al
 	jne n_eq_test
 	mov rax, QWORD [rbp - 32]
-	movzx eax, BYTE [rax]
+	movzx rax, BYTE [rax]
 	test al, al
 	je eq_test_may
 n_eq_test:
@@ -42,15 +42,15 @@ n_eq_test:
 eq_test_may:
 	cmp QWORD [rbp - 16], 0
 	jle eq_test_min
-	mov eax, 1
+	mov rax, 1
 	jmp end_1
 eq_test_min:
 	cmp QWORD [rbp - 16], 0
 	jns end_0
-	mov eax, -1
+	mov rax, -1
 	jmp end_1
 end_0:
-	mov eax, 0
+	mov rax, 0
 end_1:
 	pop rbp
 	ret

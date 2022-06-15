9f987754fc0f182ad9324c9a79fb6da4e27e2a93
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-15 18:31:37 -0500

    [UPDATE]

--- 0x05-libasm/8-strcasecmp.asm
+++ 0x05-libasm/8-strcasecmp.asm
@@ -1,3 +1,69 @@
-int asm_strcasecmp(const char *s1, const char *s2)
-{
-}
+BITS 64
+
+global asm_strcasecmp
+
+asm_strcasecmp:
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rbp - 24], rdi
+	mov QWORD [rbp - 32], rsi
+	mov DWORD [rbp - 4], 0
+	jmp dif_s1_s2
+t_1:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	cmp al, 64
+	jle false_t_1
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	cmp al, 90
+	jg false_t_1
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	add eax, 32
+	jmp true_t_1
+false_t_1:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+true_t_1:
+	mov BYTE [rbp - 5], al
+	mov rax, QWORD [rbp - 32]
+	movzx eax, BYTE [rax]
+	cmp al, 64
+	jle false_t_2
+	mov rax, QWORD [rbp - 32]
+	movzx eax, BYTE [rax]
+	cmp al, 90
+	jg false_t_2
+	mov rax, QWORD [rbp - 32]
+	movzx eax, BYTE [rax]
+	add eax, 32	
+	jmp dif_search
+false_t_2:
+	mov rax, QWORD [rbp - 32]
+	movzx eax, BYTE [rax]
+dif_search:
+	mov BYTE [rbp - 6], al
+	movsx edx, BYTE [rbp - 5]
+	movsx eax, BYTE [rbp - 6]
+	sub edx, eax
+	mov eax, edx
+	mov DWORD [rbp - 4], eax
+	add QWORD [rbp - 24], 1
+	add QWORD [rbp - 32], 1
+dif_s1_s2:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	test al, al
+	jne dif_test
+	mov rax, QWORD [rbp - 32]
+	movzx eax, BYTE [rax]
+	test al, al
+	je dif_ret
+dif_test:
+	cmp DWORD [rbp - 4], 0
+	je t_1
+dif_ret:
+	mov eax, DWORD [rbp - 4]
+	pop rbp
+	ret
--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -11,5 +11,6 @@ size_t asm_putc(int c);
 char *asm_strstr(const char *haystack, const char *needle);
 void *asm_memcpy(void *dest, const void *src, size_t n);
 size_t asm_puts(const char *str);
+int asm_strcasecmp(const char *s1, const char *s2);
 
 #endif /* _LIB_ASM_H_ */

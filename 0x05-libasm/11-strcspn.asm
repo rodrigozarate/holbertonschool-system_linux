8b60912fef5cc7b9be8b9d0c71a0e7e0ae5c0fc1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-15 19:34:36 -0500

    [UPDATE]

--- 0x05-libasm/11-strcspn.asm
+++ 0x05-libasm/11-strcspn.asm
@@ -1,3 +1,48 @@
-size_t asm_strcspn(const char *s, const char *reject)
-{
-}
+BITS 64
+
+global asm_strcspn
+
+asm_strcspn:
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rbp - 24], rdi
+	mov QWORD [rbp - 32], rsi
+	mov QWORD [rbp - 8], 0
+	mov QWORD [rbp - 16], 0
+	jmp base_loop
+cmp_loop_r:
+	mov rax, QWORD [rbp - 32]
+	mov QWORD [rbp - 16], rax
+	jmp tmp_loop
+ini_t:
+	add QWORD [rbp - 16], 1
+tmp_loop:
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	test al, al
+	je  cmp_tmp
+	mov rax, QWORD [rbp - 16]
+	movzx edx, BYTE [rax]
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	cmp dl, al
+	jne ini_t
+cmp_tmp:
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	test al, al
+	je plus_var
+	jmp return
+plus_var:
+	add QWORD [rbp - 8], 1
+	add QWORD [rbp - 24], 1
+base_loop:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	test al, al
+	jne cmp_loop_r
+return:
+	mov rax, QWORD [rbp - 8]
+	pop rbp
+	ret
+

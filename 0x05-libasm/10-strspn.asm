0be4edba418930139111d27f9a80e7e782374650
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-15 19:25:53 -0500

    [UPDATE]

--- 0x05-libasm/10-strspn.asm
+++ 0x05-libasm/10-strspn.asm
@@ -1,3 +1,47 @@
-size_t asm_strspn(const char *s, const char *accept)
-{
-}
+BITS 64
+
+global
+
+asm_strspn:
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rbp - 24], rdi
+	mov QWORD [rbp - 32], rsi
+	mov QWORD [rbp - 8], 0
+	mov QWORD [rbp - 16], 0
+	jmp base_loop
+cmp_loop:
+	mov rax, QWORD [rbp - 32]
+	mov QWORD [rbp - 16], rax
+	jmp tmp_loop
+ini_t:
+	add QWORD [rbp - 16], 1
+tmp_loop:
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	test al, al
+	je  cmp_tmp
+	mov rax, QWORD [rbp - 16]
+	movzx edx, BYTE [rax]
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	cmp dl, al
+	jne ini_t
+cmp_tmp:
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	test al, al
+	jne plus_var
+	jmp return
+plus_var:
+	add QWORD [rbp - 8], 1
+	add QWORD [rbp - 24], 1
+base_loop:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	test al, al
+	jne cmp_loop
+return:
+	mov rax, QWORD [rbp - 8]
+	pop rbp
+	ret
--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -13,5 +13,6 @@ void *asm_memcpy(void *dest, const void *src, size_t n);
 size_t asm_puts(const char *str);
 int asm_strcasecmp(const char *s1, const char *s2);
 int asm_strncasecmp(const char *s1, const char *s2, size_t n);
+size_t asm_strspn(const char *s, const char *accept);
 
 #endif /* _LIB_ASM_H_ */

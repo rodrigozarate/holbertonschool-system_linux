678e8009974db58a4c7763ad71796a568aa97ea7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-27 21:34:59 -0500

    [UPDATE]

--- 0x06-signals/1-current_handler_signal.c
+++ 0x06-signals/1-current_handler_signal.c
@@ -11,7 +11,7 @@ void (*current_handler_signal(void))(int)
 	sighandler_t handler;
 
 	handler = signal(SIGINT, SIG_IGN);
-	if (handler == SIGERR)
+	if (handler == SIG_ERR)
 		return (NULL);
 
 	if (signal(SIGINT, handler) == SIG_ERR)

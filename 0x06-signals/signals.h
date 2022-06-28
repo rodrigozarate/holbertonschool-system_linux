77110bf926c45e8022254a80a71c0a6ed73c42fd
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-27 23:38:16 -0500

    [UPDATE]

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -12,5 +12,8 @@ int trace_signal_sender(void);
 int pid_exist(pid_t pid);
 void all_in_one(void);
 int sigset_init(sigset_t *set, int *signals);
+int signals_block(int *signals);
+int signals_unblock(int *signals);
+int handle_pending(void (*handler)(int));
 
 #endif /* _SIG_H_ */

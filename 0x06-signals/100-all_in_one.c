86d25556c6e75a8e281e813496cfa5322508418a
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-27 23:20:06 -0500

    [CREATE]

---
+++ 0x06-signals/100-all_in_one.c
@@ -0,0 +1,35 @@
+#include <signal.h>
+#include <stddef.h>
+#include <signal.h>
+#include "signals.h"
+
+/**
+* signal_handler - handle signals
+* @signum: code
+* @si: pointer
+* @ucontext: context
+*/
+void signal_handler(int signum, siginfo_t *si, void *ucontext)
+{
+	(void)signum;
+	(void)ucontext;
+
+	psiginfo(si, "Caught");
+}
+
+/**
+* all_in_one - all signals
+*/
+void all_in_one(void)
+{
+	struct sigaction sigact;
+	int sig;
+
+	sigact.sa_flags |= SA_SIGINFO;
+	sigact.sa_sigaction = signal_handler;
+	for (sig = 1; sig < SIGRTMIN; sig++)
+	{
+		if (sig != SIGKILL && sig != SIGSTOP)
+			sigaction(sig, &sa, NULL);
+	}
+}

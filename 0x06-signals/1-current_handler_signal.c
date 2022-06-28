9af7450d93d16659208762c73b612957cf880cfe
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-06-27 21:21:21 -0500

    [CREATE]

---
+++ 0x06-signals/1-current_handler_signal.c
@@ -0,0 +1,21 @@
+#include <signal.h>
+#include <stddef.h>
+#include "signals.h"
+
+/**
+* current_handler_signal - return pointer to current handler signal
+* Return: pointer
+*/
+void (*current_handler_signal(void))(int)
+{
+	sighandler_t handler;
+
+	handler = signal(SIGINT, SIG_IGN);
+	if (handler == SIGERR)
+		return (NULL);
+
+	if (signal(SIGINT, handler) == SIG_ERR)
+		return (NULL);
+
+	return (handler);
+}
--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -1,7 +1,10 @@
 #ifndef _SIG_H_
 #define _SIG_H_
 
+typedef void (*sighandler_t)(int);
+
 void s_handler(int signalnum);
 int handle_signal(void);
+void (*current_handler_signal(void))(int);
 
 #endif /* _SIG_H_ */
